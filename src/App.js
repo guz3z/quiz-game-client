b602139e56d65e16984522ca5cf1a67894a7dd69
“yourGithubUsername” <your@emailadd.com>
2021-11-15 09:54:49 +0000

    add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Header } from './layout/Header/index';
 import '.style.css';
 
 
 function App() {
     return (
         <>
-            <h1>Hello World!</h1>
+            <Header />
+            {/*<Switch>
+                <Route exact path="/">
+                    <Pages.Welcome />
+                </Route>
+                <Route path="/user">
+                    <Pages.User />
+                </Route>
+                <Route path="/repo">
+                    <Pages.RepoPage />
+                </Route>
+            </Switch>
+            <Footer />*/}
         </>
     )
 }
---
+++ src/layout/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+export function Header() {
+    return(
+        <header>
+            <div className="Header">
+                <h1>Quiz name?</h1>
+            </div> 
+        </header>
+    )
+}
+
