d9d02c2deae4f7a5cc9bc7b0825bc75d97b7d01d
“yourGithubUsername” <your@emailadd.com>
2021-11-15 10:57:41 +0000

    add axios to fetch quiz api

--- src/components/CreateGame/CreateGame.js
+++ src/components/CreateGame/CreateGame.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import axios from 'axios';
 
 export function CreateGame() {
 
+    const [ userName, setUsername ] = useState("");
+    const [ category, setCategory ] = useState("");
+    const [ difficulty, setDifficulty ] = useState("");
+
+    async function getQuestions(category, difficulty) {
+        const url = `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`;
+        const { data } = await axios.get(url);
+        return data.results
+    }
+
     return (
-        
+
     )
 }
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css'
 
 export const Homepage = () => {
+
+    const [openCreateGame, setOpenCreateGame ] = useState(false);
+    const [ openJoinGame, setOpenJoinGame ] = useState(false);
+    const [ openLeaderboard, setOpenLeaderboard ] = useState(false);
+    
+
+    function handleOpenCreateGame() {
+        setOpenCreateGame(true);
+    };
+
+    function handleOpenJoinGame() {
+        setOpenJoinGame(true);
+    };
+
+    function handleOpenLeaderboard() {
+        setOpenLeaderboard(true);
+    };
 
     return(
         <>
             <main className="HomepageBody">
                 <h1>hello homepage</h1>
+                <div className="cG-btn">
+                    <button onClick={handleOpenCreateGame}>Create Game</button>
+                </div>
+                <div className="jG-btn">
+                    <button onClick={handleOpenJoinGame}>Join Game</button>
+                </div>
+                <div className="leaderboard-btn">
+                    <button onClick={handleOpenLeaderboard}>Leaderboard</button>
+                </div>
             </main>
         </>
     )
